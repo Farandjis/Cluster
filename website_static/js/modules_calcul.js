b4e82a593bd4da79af8f6ace3bc23afa3dc8931a
Bogaert Tom <146355981+BgrtTom@users.noreply.github.com>
2024-11-19 11:10:57 +0100

    src(WebSite): Modification de style et ajout de l'accessibilité sur la page d'inscription/connexion

--- website_static/js/modules_calcul.js
+++ website_static/js/modules_calcul.js
@@ -1,20 +1,45 @@
-const title_module = document.getElementsByClassName('title_module');
-const module = document.getElementsByClassName('module');
+window.onload = init;
 
+function init(){
 
-for (let i = 0; i < title_module.length; i++) {
-    title_module[i].addEventListener('click', (e) => {
-        if (module[i].classList.contains('active')){
-            title_module[i].classList.remove('active');
-            module[i].classList.remove('active');
-        }
-        else{
-            for (let j = 0; j < title_module.length; j++) {
-                title_module[j].classList.remove('active');
-                module[j].classList.remove('active');
+    const title_module = document.getElementsByClassName('title_module');
+    const l_title_module = document.getElementsByClassName('l_title_module');
+    const module = document.getElementsByClassName('module');
+
+    for (let i = 0; i < title_module.length; i++) {
+        title_module[i].addEventListener('click', (e) => {
+
+            if (module[i].classList.contains('active')){
+                title_module[i].classList.remove('active');
+                module[i].classList.remove('active');
             }
-            title_module[i].classList.add('active');
-            module[i].classList.add('active');
-        }
-    })
+            else{
+                for (let j = 0; j < title_module.length; j++) {
+                    title_module[j].classList.remove('active');
+                    module[j].classList.remove('active');
+                }
+                title_module[i].classList.add('active');
+                module[i].classList.add('active');
+            }
+        })
+
+
+        l_title_module[i].addEventListener('keydown', (e) => {
+            if (e.key === 'Enter') {
+                console.log("AHAGAGHAHAHAHA");
+                if (module[i].classList.contains('active')){
+                    title_module[i].classList.remove('active');
+                    module[i].classList.remove('active');
+                }
+                else{
+                    for (let j = 0; j < title_module.length; j++) {
+                        title_module[j].classList.remove('active');
+                        module[j].classList.remove('active');
+                    }
+                    title_module[i].classList.add('active');
+                    module[i].classList.add('active');
+                }
+            }
+        })
+    }
 }
