475872992672137a7639c50c76abb2be7e6cedd0
Matthieu <143880956+Farandjis@users.noreply.github.com>
2025-01-16 14:51:40 +0100

    Update 5_script_droits_roles.sql
    feat (grand role user insertion history) : autorisation pour insérer des données dans la table HISTORY

--- database/scripts/5_script_droits_roles.sql
+++ database/scripts/5_script_droits_roles.sql
@@ -1,3 +1,8 @@
 -- ======================== LES UTILISATEURS ========================
 -- info sur l'utilisateur
 GRANT SELECT ON Paralix.view_USER_PROFILE TO 'role_utilisateur';
+
+-- pour ajouter les informations sur les fichiers générés
+GRANT INSERT(module, filename)
+  ON Paralix.HISTORY
+  TO 'role_utilisateur';
