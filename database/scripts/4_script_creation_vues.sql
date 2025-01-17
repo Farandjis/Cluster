b6570a9a7cdbbc030e8f5fdf0478626118830412
Matthieu <143880956+Farandjis@users.noreply.github.com>
2025-01-17 23:53:32 +0100

    feat (vues) : ajout des vues de William

--- database/scripts/4_script_creation_vues.sql
+++ database/scripts/4_script_creation_vues.sql
@@ -1,4 +1,5 @@
--- Utilisateur
+-- FOR ALL THE USERS
+-- Info of the user
 CREATE OR REPLACE VIEW view_USER_PROFILE AS
 SELECT
     `Paralix`.`USERS`.`id_user` AS `id_user`,
@@ -10,3 +11,51 @@ JOIN mysql.user ON
     `user`.`User` = `Paralix`.`USERS`.`id_user`
 WHERE
     `Paralix`.`USERS`.`id_user` = SUBSTRING_INDEX(USER(), '@', 1);
+
+
+-- history of user backup history
+CREATE OR REPLACE VIEW `Paralix`.User_History AS 
+    SELECT H.time, U.login, H.module
+FROM 
+    `Paralix`.HISTORY H
+JOIN `Paralix`.`USERS` U ON
+    H.id_user = U.id_user
+WHERE 
+    U.`id_user` = SUBSTRING_INDEX(USER(), '@', 1);
+
+
+
+-- FOR ADMINS
+-- the count of backups by user and module
+CREATE OR REPLACE VIEW `Paralix`.Module_Usage_By_User AS
+    SELECT 
+    U.login, 
+    H.module, 
+    COUNT(*) AS usage_count
+FROM 
+    `Paralix`.HISTORY H
+JOIN `Paralix`.USERS U ON 
+    H.id_user = U.id_user
+GROUP BY 
+    U.login, H.module;
+
+
+-- the count of backups by module
+CREATE OR REPLACE VIEW `Paralix`.Module_Usage_Stats AS 
+    SELECT module, COUNT(*) AS total_uses 
+FROM 
+    `Paralix`.HISTORY
+GROUP BY 
+    module;
+
+
+-- the backups by all users
+CREATE OR REPLACE VIEW `Paralix`.User_Action_History AS 
+    SELECT H.time, U.login, H.module
+FROM 
+    `Paralix`.HISTORY H
+JOIN `Paralix`.USERS U 
+    ON H.id_user = U.id_user;
+
+
+
