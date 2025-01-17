4e08ec5a6c492771e8f21adc1b2358528959f1e9
Matthieu <143880956+Farandjis@users.noreply.github.com>
2025-01-18 00:01:20 +0100

    feat (droits db) : ajout du code de William + 1 à moi celle de delete dans l'historique de l'utilisateur

--- database/scripts/5_script_droits_roles.sql
+++ database/scripts/5_script_droits_roles.sql
@@ -6,3 +6,20 @@ GRANT SELECT ON Paralix.view_USER_PROFILE TO 'role_utilisateur';
 GRANT INSERT(module, filename)
   ON Paralix.HISTORY
   TO 'role_utilisateur';
+
+-- L'admin peut consulter ses dernières sauvegarde sur le site
+Grant Select On Paralix.User_History To 'role_utilisateur';
+
+-- L'admin peut supprimer ses sauvegardes
+Grant Delete on Paralix.User_History to 'role_utilisateur';
+
+
+
+-- ======================== LES ADMINS ========================
+-- L'admin est un utilisateur
+Grant role_utilisateur To role_admin;
+
+-- L'admin peut consulter l'activité des sauvegardes sur le site
+Grant Select On Paralix.Module_Usage_By_User To 'role_admin';
+Grant Select On Paralix.Module_Usage_Stats To 'role_admin';
+Grant Select On Paralix.User_Action_History To 'role_admin';
