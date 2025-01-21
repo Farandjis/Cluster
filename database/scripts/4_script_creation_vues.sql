2403e1be1f78e5bf21da0f186730875e3ed63ff3
Matthieu <143880956+Farandjis@users.noreply.github.com>
2025-01-21 12:44:03 +0100

    feat (vues info user et vues update settings)  : transfert des variables vers la vue update setting

--- database/scripts/4_script_creation_vues.sql
+++ database/scripts/4_script_creation_vues.sql
@@ -4,13 +4,22 @@ CREATE OR REPLACE VIEW view_USER_PROFILE AS
 SELECT
     `Paralix`.`USERS`.`id_user` AS `id_user`,
     `Paralix`.`USERS`.`login` AS `login`,
-    `user`.`default_role` AS `role`,
-    `Paralix`.`USERS`.`saveUserSettings_AutoSave` AS saveUserSettings_AutoSave,
-    `Paralix`.`USERS`.`saveUserSettings_AuteDeletion` AS saveUserSettings_AuteDeletion
+    `user`.`default_role` AS `role`
 FROM
     `Paralix`.`USERS`
 JOIN mysql.user ON
     `user`.`User` = `Paralix`.`USERS`.`id_user`
+WHERE
+    `Paralix`.`USERS`.`id_user` = SUBSTRING_INDEX(USER(), '@', 1);
+
+-- update settings 
+CREATE OR REPLACE VIEW view_USER_SETTINGS AS
+SELECT
+    `Paralix`.`USERS`.`id_user` AS `id_user`,
+    `Paralix`.`USERS`.`saveUserSettings_AutoSave` AS saveUserSettings_AutoSave,
+    `Paralix`.`USERS`.`saveUserSettings_AuteDeletion` AS saveUserSettings_AuteDeletion
+FROM
+    `Paralix`.`USERS`
 WHERE
     `Paralix`.`USERS`.`id_user` = SUBSTRING_INDEX(USER(), '@', 1);
 
